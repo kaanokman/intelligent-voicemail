08534f9d03f635e1559607016a8e85a1246a3925
Kaan Okman <kaanokman@gmail.com>
2026-02-04 16:20:27 -0500

    test

--- components/GoogleLoginButton.tsx
+++ components/GoogleLoginButton.tsx
@@ -41,7 +41,7 @@ export default function GoogleLoginButton() {
     return (ready ?
         <div className='google-login-wrapper'>
             <GoogleLogin
-                width={'100%'}
+                width={350}
                 onSuccess={(credentialResponse) => { void handleSignInWithGoogle(credentialResponse); }}
                 onError={() => {
                     console.log('Login Failed');
